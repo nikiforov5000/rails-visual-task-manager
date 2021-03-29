09e4f22e8121cf246e485f875f184938610f48e2
boris nikiforov <nikiforov5000@gmail.com>
2021-03-29 19:34:20 +0300

    ARCH: added new and create method for day_pack

--- app/controllers/day_packs_controller.rb
+++ app/controllers/day_packs_controller.rb
@@ -1,2 +1,22 @@
 class DayPacksController < ApplicationController
+  def new
+    @day_pack = DayPack.new
+  end
+
+  def create
+    @day_pack = DayPack.new(day_pack_params)
+    if @day_pack.save
+      flash[:success] = "DayPack successfully created"
+      redirect_to @day_pack
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end
+
+  private
+
+  def day_pack_params
+    params.require(:day_pack).permit(:title)
+  end
 end
