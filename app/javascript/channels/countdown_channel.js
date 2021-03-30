e73b941b32da5eb90b2d07d0331b50520bccd41a
boris nikiforov <nikiforov5000@gmail.com>
2021-03-30 15:57:53 +0300

    FEAT: pause and start work

--- app/javascript/channels/countdown_channel.js
+++ app/javascript/channels/countdown_channel.js
@@ -6,12 +6,20 @@ countTimers();
 function countTimers() {
   timersCount++;
 
-  let count = 26;
+  let count = document.querySelector('#count').innerHTML;
   const counter = setInterval(timer, 1000);
 
   function timer() {
     if (!pause) { //do something if not paused
       count = count - 1;
+      
+      // Time calculations for days, hours, minutes and seconds
+      let days = Math.floor(count / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((count % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((count % (1000 * 60)) / 1000);
+
+
       if (count < 0) {
         clearInterval(counter);
         setTimeout(countTimers, 5000); //start count from 26 again
@@ -25,10 +33,12 @@ function countTimers() {
   document.getElementById("countTimers").innerHTML = timersCount;
 }
 
-document.getElementById('pause').addEventListener('click', function () {
-	pause = true;
-});
-
-document.getElementById('resume').addEventListener('click', function () {
-	pause = false;
+document.getElementById('test').addEventListener('click', (event) => {
+  if ( pause ) {
+  event.currentTarget.innerHTML = "Pause";
+  }
+  else {
+  event.currentTarget.innerHTML = "Start";
+  }
+	pause = !pause;
 });
